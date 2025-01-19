bab160654d89450996fd30b628d388625f2a3e47
Kennedy <kennedyfigueiredodelima@gmail.com>
2025-01-19 09:23:15 -0300

    agora vai

--- my_flask_app/app/main.py
+++ my_flask_app/app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return jsonify(message="Hello, NTT DATA1!")
+    return jsonify(message="Hello, NTT DATA!")
 
 if __name__ == '__main__':
     app.run(debug=True)
