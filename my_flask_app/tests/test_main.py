a041a77cf8c97d10ecfaa54761695bc4a9fb23a2
Kennedy <kennedyfigueiredodelima@gmail.com>
2025-01-18 21:41:25 -0300

    teste

---
+++ my_flask_app/tests/test_main.py
@@ -0,0 +1,12 @@
+import pytest
+from app.main import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_hello(client):
+    rv = client.get('/')
+    json_data = rv.get_json()
+    assert json_data['message'] == 'Hello, World!'
