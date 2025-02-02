101a971dd603294efcb7f87e7eee2c252a8b502e
Kennedy <kennedyfigueiredodelima@gmail.com>
2025-02-01 23:35:31 -0300

    teste 1

--- tests/test_main.py
+++ tests/test_main.py
@@ -12,5 +12,5 @@ def test_home_page(client):
     """Testa se a rota '/' retorna a mensagem correta"""
     response = client.get('/')
     assert response.status_code == 200  # Verifica se a resposta foi bem-sucedida
-    assert b'Hello, NTT DATA' in response.data  # Verifica se o conteúdo esperado está presente
+    assert b'Teste de deploy' in response.data  # Verifica se o conteúdo esperado está presente
 
